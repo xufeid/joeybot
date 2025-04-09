1f9311df7a6f5bc7d9974aac6c54854a5fea45f2
XeqtRrrrrr <joey@northbund.capital>
2025-04-09 22:20:53 +0800

    referal link type update

--- wallets-monitor/src/strategy/messageTemplate.js
+++ wallets-monitor/src/strategy/messageTemplate.js
@@ -48,8 +48,9 @@ export function createMsg(tokenInfo, analysis) {
 ${smartMoneyCount} wallets bought $${tokenInfo.symbol}
 
 ${formatSmartMoney(analysis)}
-<a href="https://gmgn.ai/sol/token/${GMGN_REFERAL_CODE}_${tokenInfo.address}">GMGN</a>${tokenInfo.website ? ` | <a href="https://t.me/GMGN_sol_bot?start=i_${GMGN_REFERAL_CODE}_c_${tokenInfo.address}">GMGN TG Bot</a>
-<a href="https://dexscreener.com/solana/${tokenInfo.address}">DexScreener</a> |  <a href="${tokenInfo.website}">Website</a>` : ''}${tokenInfo.twitter ? ` | <a href="${tokenInfo.twitter}">Twitter</a>` : ''}
+
+<a href="https://dexscreener.com/solana/${tokenInfo.address}">DexScreener</a> | ${tokenInfo.website ? ` | <a href="${tokenInfo.website}">Website</a>` : ''}${tokenInfo.twitter ? ` | <a href="${tokenInfo.twitter}">Twitter</a>` : ''}
+<a href="https://gmgn.ai/sol/token/${GMGN_REFERAL_CODE}_${tokenInfo.address}">GMGN</a> | <a href="https://t.me/GMGN_sol_bot?start=i_${GMGN_REFERAL_CODE}_c_${tokenInfo.address}">GMGN TG Bot</a> 
 `.trim();
 }
 
